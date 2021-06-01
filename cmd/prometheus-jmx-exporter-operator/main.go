0c7e12c4d9704dcb4259da4dd5418d3ada684a34
Eric Baranowski <eric@kulado.com>
2021-06-01 03:48:09 -0400

    init

---
+++ cmd/prometheus-jmx-exporter-operator/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"runtime"
+
+	stub "github.com/banzaicloud/prometheus-jmx-exporter-operator/pkg/stub"
+	sdk "github.com/operator-framework/operator-sdk/pkg/sdk"
+	sdkVersion "github.com/operator-framework/operator-sdk/version"
+
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+func printVersion() {
+	logrus.Infof("Go Version: %s", runtime.Version())
+	logrus.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
+	logrus.Infof("operator-sdk Version: %v", sdkVersion.Version)
+}
+
+func main() {
+	printVersion()
+	namespace := os.Getenv("OPERATOR_NAMESPACE")
+
+	sdk.Watch("banzaicloud.com/v1alpha1", "PrometheusJmxExporter", namespace, 0)
+	sdk.Watch("v1", "Pod", namespace, 0)
+	sdk.Handle(stub.NewHandler())
+	sdk.Run(context.TODO())
+}
